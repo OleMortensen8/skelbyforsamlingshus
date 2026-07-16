3ef043b3dfa94a6d41c04f99a8330f939fb13ebe
Ole V. S. Mortensen <dev@dbc5radio.dk>
2026-07-16 02:41:04 +0200

    Refactor: Obfuscate email and phone number in board member details

--- bestyrelse.php
+++ bestyrelse.php
@@ -8,7 +8,8 @@
                 <div class="board-member">
                     <h3>BestyrelsesFormand</h3>
                     <p>Per Skov Mortensen</p>
-                    <p>formand@skelby-forsamlingshus.dk</p>
+                    <p>&#x66;&#x6f;&#x72;&#x6d;&#x61;&#x6e;&#x64;&#x40;&#x73;&#x6b;&#x65;&#x6c;&#x62;&#x79;&#x2d;&#x66;&#x6f;&#x72;&#x73;&#x61;&#x6d;&#x6c;&#x69;&#x6e;&#x67;&#x73;&#x68;&#x75;&#x73;&#x2e;&#x64;&#x6b;</p>
+                    <p>&#x2b;&#x34;&#x35;&#x34;&#x30;&#x31;&#x33;&#x38;&#x30;&#x35;&#x35;</p>
                 </div>
 
                 <div class="board-member">
