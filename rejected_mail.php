feed5aaab4394a8b420b2aee7cd6922ba9391f22
Ole V. S. Mortensen <dev@dbc5radio.dk>
2026-07-19 01:31:09 +0200

    Fix: Add missing newline at the beginning of phpmailer_2.php

--- rejected_mail.php
+++ rejected_mail.php
@@ -7,32 +7,32 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 try {
-//Create a new PHPMailer instance
-$mail3 = new PHPMailer(true);
-//Server settings
-$mail3->SMTPDebug = 0;                 // Enable verbose debug output
-$mail3->isSMTP();                                            // Send using SMTP
-$mail3->Host       = 'websmtp.simply.com';                  // Set the SMTP server to send through
-$mail3->SMTPAuth   = true;                                   // Enable SMTP authentication
-$mail3->Username   = 'ue334094@skelby-forsamlingshus.dk';                                      // SMTP username
-$mail3->Password   = '***REMOVED-LEAKED-SMTP-PASSWORD***';                                     // SMTP password
-$mail3->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;       // Enable TLS encryption; PHPMailer::ENCRYPTION_SMTPS also accepted
-$mail3->Port       = 587;
-$mail3->CharSet = 'UTF-8';
-$mail3->isHTML(true);
-//Set who the message is to be sent from
-$mail3->setFrom('ue334094@skelby-forsamlingshus.dk', 'Forsamlingshuset');
-$mail3->addReplyTo(getenv('MAIL_REPLY_TO') ?: 'kasserer@skelby-forsamlingshus.dk', 'Kasserer');
-$mail3->addAddress($mailer, 'Gæst');
-$bccDev = getenv('MAIL_BCC_DEV') ?: '';
-if ($bccDev !== '') {
-    $mail3->addBCC($bccDev);
-}
-$mail3->Subject = "Anullering  af Bestilling";
-$mail3->Body = 'Kære '. $name . 
-'<br/> Vi har bekræftet din booking til ' . $dato . 'er anulleret af vores reræsentant.<br/>' .
-'M.v.h. fra  Skelby forsamlingshus på Falster';
-$mail3->send();
-}catch(Exception $e){
+    //Create a new PHPMailer instance
+    $mail3 = new PHPMailer(true);
+    //Server settings
+    $mail3->SMTPDebug = 0;                 // Enable verbose debug output
+    $mail3->isSMTP();                                            // Send using SMTP
+    $mail3->Host       = 'websmtp.simply.com';                  // Set the SMTP server to send through
+    $mail3->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail3->Username   = 'ue334094@skelby-forsamlingshus.dk';                                      // SMTP username
+    $mail3->Password   = '***REMOVED-LEAKED-SMTP-PASSWORD***';                                     // SMTP password
+    $mail3->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;       // Enable TLS encryption; PHPMailer::ENCRYPTION_SMTPS also accepted
+    $mail3->Port       = 587;
+    $mail3->CharSet = 'UTF-8';
+    $mail3->isHTML(true);
+    //Set who the message is to be sent from
+    $mail3->setFrom('ue334094@skelby-forsamlingshus.dk', 'Forsamlingshuset');
+    $mail3->addReplyTo(getenv('MAIL_REPLY_TO') ?: 'kasserer@skelby-forsamlingshus.dk', 'Kasserer');
+    $mail3->addAddress($mailer, 'Gæst');
+    $bccDev = getenv('MAIL_BCC_DEV') ?: '';
+    if ($bccDev !== '') {
+        $mail3->addBCC($bccDev);
+    }
+    $mail3->Subject = "Anullering  af Bestilling";
+    $mail3->Body = 'Kære ' . $name .
+        '<br/> Vi har bekræftet din booking til ' . $dato . 'er anulleret af vores reræsentant.<br/>' .
+        'M.v.h. fra  Skelby forsamlingshus på Falster';
+    $mail3->send();
+} catch (Exception $e) {
     echo "Message could not be sent. mailer3 Error: {$mail3->ErrorInfo}";
 }
