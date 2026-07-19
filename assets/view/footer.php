de700a5b4cbeafb98dc78ca58b7048c25ade03c1
Ole V. S. Mortensen <dev@dbc5radio.dk>
2026-07-20 01:35:14 +0200

    Enhance footer styling: add footer-link class for improved link styling and adjust image max-height

--- assets/view/footer.php
+++ assets/view/footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer>
     <p>CVR nr.: 33334443</p>
-    <p>&copy; 2020-<?php echo $HereAndNow; ?> Denne hjemmeside tilhører Skelby, Falster <br /> lavet af <a href="https://dbc5radio.dk/portfolio.php">Ole Vølund Skov Mortensen</a></p>
+    <p>&copy; 2020-<?php echo $HereAndNow; ?> Denne hjemmeside tilhører Skelby, Falster <br /> lavet af <a class="footer-link" href="https://dbc5radio.dk/portfolio.php">Ole Vølund Skov Mortensen</a></p>
 </footer>
 </body>
 
