e0c3584313f2a59d90354f8bb1f5ef05c6965640
Ole V. S. Mortensen <dev@dbc5radio.dk>
2026-07-20 00:55:27 +0200

    Fix: Add hyperlink to portfolio in footer section

--- assets/view/footer.php
+++ assets/view/footer.php
@@ -1,7 +1,8 @@
 </div>
 <footer>
     <p>CVR nr.: 33334443</p>
-    <p>&copy; 2020-<?php echo $HereAndNow; ?> Denne hjemmeside tilhører Skelby, Falster <br/> lavet af Ole Vølund Skov Mortensen</p>
+    <p>&copy; 2020-<?php echo $HereAndNow; ?> Denne hjemmeside tilhører Skelby, Falster <br /> lavet af <a href="https://dbc5radio.dk/portfolio.php">Ole Vølund Skov Mortensen</a></p>
 </footer>
 </body>
+
 </html>
