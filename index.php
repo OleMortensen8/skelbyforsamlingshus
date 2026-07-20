d10f267508c69df1d2c7dfae7ed6f06d8abb27c4
Ole V. S. Mortensen <dev@dbc5radio.dk>
2026-07-20 02:24:33 +0200

    Enhance styling: add arrow indicators for layout transitions and improve link styling in contact section

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <?php include 'assets/view/header.php'; ?>
-        <main>
-            <div id="centerColumn" style="position: relative;">
-            
+<main>
+	<div id="centerColumn" style="position: relative;">
 
-<div id="twoColumn">
-            	<div id="column2">
-                <iframe width="100%" height="350" src="https://www.openstreetmap.org/export/embed.html?bbox=11.892968416213991%2C54.63258619607408%2C11.910885572433473%2C54.63779608414236&amp;layer=mapnik&amp;marker=54.63519122354269%2C11.90192699432373" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=54.63519&amp;mlon=11.90193#map=17/54.63519/11.90193&amp;layers=N">Vis større kort</a></small>
-              </div>    
 
-			  <div class="arrow-up"></div>   
-			
-			  <div style="width:90%; height:350px;overflow: hidden;">
-                  <img style="width:100%; height:100%;margin-left:25px; object-fit: cover;object-position: right;"
-                       src="/assets/img/skelby/setting3.jpg" alt=""
-                       srcset="">
-			  </div>
-			  <div style="width:73%;text-align:center; margin:auto;margin-left: 66px;">
-			<p style="font-size: 16px; line-height: 1.5;"><b>Forsamlingshus i Skelby</b><br>
-			<address style="font-style: normal;">Gl. Landevej 66, 4874 Gedser</address>
-			<a style="color: blue;" href="tel:+4512345678">+45 22143669</a>
-			<!--<<a style="color: blue;" href="mailto:info@skelbyforsamlingshus.dk">info@skelbyforsamlingshus.dk</a>-->
-			</p>
+		<div id="twoColumn">
+			<div id="column2">
+				<iframe width="100%" height="350" src="https://www.openstreetmap.org/export/embed.html?bbox=11.892968416213991%2C54.63258619607408%2C11.910885572433473%2C54.63779608414236&amp;layer=mapnik&amp;marker=54.63519122354269%2C11.90192699432373" style="border: 1px solid black"></iframe><br /><small><a href="https://www.openstreetmap.org/?mlat=54.63519&amp;mlon=11.90193#map=17/54.63519/11.90193&amp;layers=N">Vis større kort</a></small>
+			</div>
+
+			<div class="arrow-up"></div>
+
+			<div style="width:90%; height:350px;overflow: hidden;">
+				<img style="width:100%; height:100%;margin-left:25px; object-fit: cover;object-position: right;"
+					src="/assets/img/skelby/setting3.jpg" alt=""
+					srcset="">
+			</div>
+			<div style="width:73%;text-align:center; margin:auto;margin-left: 66px;">
+				<p style="font-size: 16px; line-height: 1.5;"><b>Forsamlingshus i Skelby</b><br>
+				<address style="font-style: normal;">Gl. Landevej 66, 4874 Gedser</address>
+				<a class="subtle-link" href="&#x74;&#x65;&#x6c;&#x3a;&#x2b;&#x34;&#x35;&#x32;&#x32;&#x31;&#x34;&#x33;&#x36;&#x36;&#x39;">&#x2b;&#x34;&#x35;&#x20;&#x32;&#x32;&#x31;&#x34;&#x33;&#x36;&#x36;&#x39;</a>
+				<!--<<a style="color: blue;" href="mailto:info@skelbyforsamlingshus.dk">info@skelbyforsamlingshus.dk</a>-->
+				</p>
+			</div>
+
+
 		</div>
 
 
-	</div>
-
-
-        </main>
-<?php include 'assets/view/footer.php'; ?>  
+</main>
+<?php include 'assets/view/footer.php'; ?>
--- kontakt.php
+++ kontakt.php
@@ -7,17 +7,17 @@
             <div class="contact-info">
                 <div class="contact-section">
                     <h3>Udlejning</h3>
-                    <p>Tel: +4522143669</p>
+                    <p>Tel: <a href="&#x74;&#x65;&#x6c;&#x3a;&#x2b;&#x34;&#x35;&#x32;&#x30;&#x38;&#x33;&#x35;&#x36;&#x34;&#x37;">&#x2b;&#x34;&#x35;&#x20;&#x32;&#x30;&#x20;&#x38;&#x33;&#x20;&#x35;&#x36;&#x20;&#x34;&#x37;</a></p>
                 </div>
 
                 <div class="contact-section">
                     <h3>Regnskab</h3>
-                    <p>Tel: +4553571559</p>
+                    <p>Tel: <a href="&#x74;&#x65;&#x6c;&#x3a;&#x2b;&#x34;&#x35;&#x32;&#x30;&#x38;&#x33;&#x35;&#x36;&#x34;&#x37;">&#x2b;&#x34;&#x35;&#x20;&#x32;&#x30;&#x20;&#x38;&#x33;&#x20;&#x35;&#x36;&#x20;&#x34;&#x37;</a></p>
                 </div>
 
                 <div class="contact-section">
                     <h3>Andet</h3>
-                    <p>Tel: +4540138055</p>
+                    <p>Tel: <a href="&#x74;&#x65;&#x6c;&#x3a;&#x2b;&#x34;&#x35;&#x34;&#x30;&#x31;&#x33;&#x38;&#x30;&#x35;&#x35;">&#x2b;&#x34;&#x35;&#x20;&#x34;&#x30;&#x20;&#x31;&#x33;&#x20;&#x38;&#x30;&#x20;&#x35;&#x35;</a></p>
                 </div>
 
                 <div class="address-section">
